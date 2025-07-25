0020bd035c1a411fb60758bb6a3b6b1d3bb680c6
Karthika Kalimuthu <karthikabeaero2003@gmail.com>
2025-07-25 12:17:23 +0530

    Python Practices

---
+++ 25-07-2025/if.py
@@ -0,0 +1,18 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You are too young to vote")
+
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Grade F")
