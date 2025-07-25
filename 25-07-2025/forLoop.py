6c7a9d050cddc514f210bc2f4cd469b769f61562
Karthika Kalimuthu <karthikabeaero2003@gmail.com>
2025-07-25 12:34:41 +0530

    Python Practices

---
+++ 25-07-2025/forLoop.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print("Number: ",i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(1,10):
+    if i == 5:
+        continue
+    if i == 8:
+        break
+    print(i)
