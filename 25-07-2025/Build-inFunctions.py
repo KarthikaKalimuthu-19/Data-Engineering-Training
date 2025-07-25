b4bc07fbec90e606cd1f8d47bed1e8cb8102fd00
Karthika Kalimuthu <karthikabeaero2003@gmail.com>
2025-07-25 16:05:18 +0530

    Python Practices

---
+++ 25-07-2025/Build-inFunctions.py
@@ -0,0 +1,24 @@
+name = "karthika"
+print(len(name))
+
+print(type(5))
+print(type("Hii"))
+
+age = "12"
+print(int(age) + 5)
+
+#name = input("Enter name: ")
+#print(f"Hello {name}!")
+
+num = [3,8,2]
+print(sum(num))    #13
+print(max(num))    #8
+print(min(num))    #2
+
+names = ["karthika", "meena", "asha"]
+print(sorted(names))
+
+print(abs(-6))    #6
+
+print(round(3.75))    #4
+print(round(3.75756, 2))    #3.76
---
+++ 25-07-2025/function-1.py
@@ -0,0 +1,22 @@
+def greet_user(name):
+    print(f"Hello {name}! Welcome to Hexaware.")
+
+greet_user("Karthika")
+
+###############
+
+def add(a,b):
+    return a+b
+
+result = add(10,20)
+print("Add result is: ",result)
+
+###############
+
+def power(base, exponent=2):
+    return base**exponent
+
+ans = power(4)
+print(ans)
+ans1 = power(2,3)
+print(ans1)
