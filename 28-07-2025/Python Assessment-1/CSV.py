77a2211ffdd7efc510632399d0ea77fc59fd67bf
Karthika Kalimuthu <karthikabeaero2003@gmail.com>
2025-07-28 17:30:12 +0530

    Assessment-1

--- 28-07-2025/Python Assessment-1/CSV.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('students.csv')
-
-df['Age'] = df['Age'].fillna(df['Age'].mean())
-
-df['Score'] = df['Score'].fillna('0')
-
-df.to_csv('students_cleaned.csv', index=False)
--- 28-07-2025/Python Assessment-1/JSON.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import json
-
-df = pd.read_csv("students_cleaned.csv")
-df.to_json("students.json", orient="records", indent=2)
