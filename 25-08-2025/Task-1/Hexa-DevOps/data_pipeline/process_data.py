aa3a37778148896d8095d5d44e517b5d3138155e
Karthika Kalimuthu <karthikabeaero2003@gmail.com>
2025-08-25 11:36:03 +0530

    Task-1 Completed

---
+++ 25-08-2025/Task-1/Hexa-DevOps/data_pipeline/process_data.py
@@ -0,0 +1,18 @@
+import json
+import fetch_data
+
+def process():
+    with open("raw_data.json", "r") as f:
+        data = json.load(f)
+
+    high_scorers = [s for s in data["students"] if s["marks"] > 80]
+
+    with open("processed_data.json", "w") as f:
+        json.dump(high_scorers, f, indent=4)
+
+    print("Processed data saved to processed_data.json")
+
+if __name__ == "__main__":
+    fetch_data.fetch() 
+    process()
+
